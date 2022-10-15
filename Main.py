7add690fe2e69b08fb0d2213f08d460bb752cabf
Dominic <dmuze101@gmail.com>
2022-10-14 20:07:30 -0400

    display with white screen

--- Main.py
+++ Main.py
@@ -1,1 +1,23 @@
-print("is this still working?")
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode([500, 500])
+
+# Run until the user asks to quit
+running = True
+while running:
+
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the background with white
+    screen.fill((255, 255, 255))
+
+    # Flip the display (updates the display with changes made to the screen)
+    pygame.display.flip()
+
+# Done! Time to quit.
+pygame.quit()
